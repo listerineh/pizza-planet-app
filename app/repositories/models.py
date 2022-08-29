2a85c022dc24871a69c3c2eed982c612b3d7ca3e
listerineh <jsalvarez4@espe.edu.ec>
2022-08-29 17:10:10 -0500

    add: simple model for beverage

--- app/repositories/models.py
+++ app/repositories/models.py
@@ -34,4 +34,11 @@ class OrderDetail(db.Model):
     ingredient_price = db.Column(db.Float)
     order_id = db.Column(db.Integer, db.ForeignKey('order._id'))
     ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredient._id'))
-    ingredient = db.relationship('Ingredient', backref=db.backref('ingredient'))
+    ingredient = db.relationship(
+        'Ingredient', backref=db.backref('ingredient'))
+
+
+class Beverage(db.Model):
+    _id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    price = db.Column(db.Float, nullable=False)

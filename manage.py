9ad171fbf63fe64d1704e77c3eefbab20360d656
listerineh <jsalvarez4@espe.edu.ec>
2022-08-31 00:43:14 -0500

    add: commands to drop and fill the database

--- manage.py
+++ manage.py
@@ -1,8 +1,10 @@
 
 
 import pytest
+import subprocess
 from flask.cli import FlaskGroup
 from flask_migrate import Migrate
+from flask_seeder import FlaskSeeder
 
 from app import flask_app
 from app.plugins import db
@@ -14,6 +16,22 @@ manager = FlaskGroup(flask_app)
 
 migrate = Migrate()
 migrate.init_app(flask_app, db)
+
+seeder = FlaskSeeder()
+seeder.init_app(flask_app, db)
+
+
+@manager.command('drop_db', with_appcontext=False)
+def drop_db():
+    subprocess.run('python ./manage.py db downgrade base')
+    subprocess.run('python ./manage.py db upgrade')
+    print('Database dropped succesfully!')
+
+
+@manager.command('fill_db', with_appcontext=False)
+def fill_db():
+    subprocess.run('python ./manage.py seed run --root app/seeds')
+    print('Database filled succesfully!')
 
 
 @manager.command('test', with_appcontext=False)

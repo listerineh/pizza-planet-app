bfefb8a08a13bd2cb1607d178423ca9b08590d51
listerineh <jsalvarez4@espe.edu.ec>
2022-08-31 14:15:09 -0500

    add: order creation in seeder

--- app/seeds/db_seeder.py
+++ app/seeds/db_seeder.py
@@ -1,7 +1,7 @@
 from flask_seeder import Seeder
-from random import randint
+from random import randint, choice
 
-from app.seeds.utils.functions import get_orders_order, get_random_dates
+from app.seeds.utils.functions import get_orders_order, get_random_dates, get_random_items, calculate_total_order_price
 from app.seeds.fakers import *
 from app.seeds.data.sizes import *
 from app.seeds.data.lists import client_names
@@ -10,50 +10,97 @@ from app.test.utils.functions import get_random_phone
 
 class DBSeeder(Seeder):
 
-    def save(self, data: list, name: str):
+    def save(self, data: list):
         for item in data:
             self.db.session.add(item)
-        print(
-            f'{len(data)} {name} succesfully added to the database!')
 
     def run(self):
 
         # Creating data
 
-        sizes = create_size_faker().create(sizes_len)
-        ingredients = create_ingredient_faker().create(ingredients_len)
-        beverages = create_beverage_faker().create(beverages_len)
+        sizes_faker = create_size_faker().create(sizes_len)
+        ingredients_faker = create_ingredient_faker().create(ingredients_len)
+        beverages_faker = create_beverage_faker().create(beverages_len)
 
         # Creating orders
 
         orders_order = get_orders_order(users_len, orders_len)
-        counter = 1
-        index = 0
+        order_counter = 1
+        ingredients_counter = 1
+        beverages_counter = 1
+        users_index = 0
 
         dates = get_random_dates(orders_len)
         orders = []
+        ingredients_detail = []
+        beverages_detail = []
 
         for order in orders_order:
-            new_order = {
-                '_id': counter,
-                'client_name': client_names[index],
-                'client_dni': client_names[index],
-                'client_address': client_names[index],
-                'client_phone': get_random_phone(),
-                'date': dates[counter-1],
-                'total_price': 0,
-                'size_id': randint(1, sizes_len),
-            }
+
+            random_number = get_random_phone()
 
             for _ in range(order):
-                orders.append(create_order_faker(new_order).create()[0])
-                counter += 1
 
-            index += 1
+                random_size = choice(sizes_faker)
+
+                new_order = {
+                    '_id': order_counter,
+                    'client_name': client_names[users_index],
+                    'client_dni': client_names[users_index],
+                    'client_address': client_names[users_index],
+                    'client_phone': random_number,
+                    'date': dates[order_counter-1],
+                    'total_price': 0,
+                    'size_id': random_size._id,
+                }
+
+                faker_order = create_order_faker(new_order).create()[0]
+                random_ingredients = get_random_items(ingredients_faker)
+                random_beverages = get_random_items(beverages_faker)
+
+                for random_ingredient in random_ingredients:
+                    new_ingredients_detail = {
+                        '_id': ingredients_counter,
+                        'ingredient_price': random_ingredient.price,
+                        'order_id': faker_order._id,
+                        'ingredient_id': random_ingredient._id,
+                    }
+
+                    ingredients_detail.append(create_ingredients_detail_faker(
+                        new_ingredients_detail).create()[0])
+
+                    ingredients_counter += 1
+
+                for random_beverage in random_beverages:
+                    new_beverages_detail = {
+                        '_id': beverages_counter,
+                        'beverage_price': random_beverage.price,
+                        'order_id': faker_order._id,
+                        'beverage_id': random_beverage._id,
+                    }
+
+                    beverages_detail.append(create_beverages_detail_faker(
+                        new_beverages_detail).create()[0])
+
+                    beverages_counter += 1
+
+                faker_order.total_price = calculate_total_order_price(
+                    random_size.price, ingredients_detail, beverages_detail)
+
+                self.save(ingredients_detail)
+                self.save(beverages_detail)
+
+                ingredients_detail.clear()
+                beverages_detail.clear()
+
+                orders.append(faker_order)
+                order_counter += 1
+
+            users_index += 1
 
         # Savind the data
 
-        self.save(sizes, 'sizes')
-        self.save(ingredients, 'ingredients')
-        self.save(beverages, 'beverages')
-        self.save(orders, 'orders')
+        self.save(sizes_faker)
+        self.save(ingredients_faker)
+        self.save(beverages_faker)
+        self.save(orders)

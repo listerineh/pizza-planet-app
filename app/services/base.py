cd679542e60e1e96d2f3eccc1d3770854c8e3f9d
Sebastian Alvarez <jsalvarez4@espe.edu.ec>
2022-09-02 02:36:15 -0500

    refactor: function syntax

--- app/services/base.py
+++ app/services/base.py
@@ -6,8 +6,11 @@ from app.common.http_methods import GET, POST, PUT
 
 def run_service(EntityController, method, request: Optional[Request] = None, id: Optional[int] = None):
 
-    entity, error = EntityController.get_by_id(id) if id else EntityController.get_all() if method == GET else EntityController.create(
-        request) if method == POST else EntityController.update(request) if method == PUT else EntityController.get_report()
+    entity, error = EntityController.get_by_id(id) if id \
+        else EntityController.get_all() if method == GET \
+        else EntityController.create(request) if method == POST \
+        else EntityController.update(request) if method == PUT \
+        else EntityController.get_report()
 
     response = entity if not error else {'error': error}
     status_code = 200 if entity else 404 if not error else 400

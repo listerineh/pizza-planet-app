e921b6837d77d55e6dffe20a67a39f2fd4efd1bf
Sebastian Alvarez <jsalvarez4@espe.edu.ec>
2022-09-03 23:38:49 -0500

    fix: delete usued stuff to increase the coverage

--- app/test/fixtures/order.py
+++ app/test/fixtures/order.py
@@ -24,19 +24,6 @@ def client_data():
 
 
 @pytest.fixture
-def order(create_ingredients, create_beverages, create_size, client_data) -> dict:
-    ingredients = [ingredient.get('_id') for ingredient in create_ingredients]
-    beverages = [beverage.get('_id') for beverage in create_beverages]
-    size_id = create_size.get('_id')
-    return {
-        **client_data_mock(),
-        'ingredients': ingredients,
-        'beverages': beverages,
-        'size_id': size_id
-    }
-
-
-@pytest.fixture
 def create_order(client, order_uri, create_ingredients, create_beverages, create_sizes) -> list:
     ingredients = [ingredient.get('_id') for ingredient in create_ingredients]
     beverages = [beverage.get('_id') for beverage in create_beverages]
--- app/test/utils/functions.py
+++ app/test/utils/functions.py
@@ -23,10 +23,6 @@ def shuffle_list(choices: list) -> list:
     return choice_copy
 
 
-def get_random_email() -> str:
-    return f"{get_random_string()}@{get_random_choice(['hotmail.com', 'gmail.com', 'test.com'])}"
-
-
 def get_random_sequence(length: int = 10) -> str:
     digits = list(map(str, range(10)))
     sequence = [digits[random.randint(0, 9)] for _ in range(length)]
